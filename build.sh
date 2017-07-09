35fcd8f237f0b276057bbf3bd884cfcb9b14fb31
Mark Abramov <markizko@gmail.com>
2017-07-09 16:13:39 +0300

    Add explicit id to manifest

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 mkdir -p build
-cd chrome
-rm -f ../build/mokum-chrome-extension.zip
-zip -r ../build/mokum-chrome-extension.zip .
+mkdir -p tmp
+cp -R chrome tmp/
+cd tmp/chrome/
+rm -f ../../build/mokum-chrome-extension.zip
+zip -r ../../build/mokum-chrome-extension.zip .
